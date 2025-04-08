fe34d31c9ffe01d56e18e2e734a96b140ff1f1a4
DieBarRo <105682742+DieBarRo@users.noreply.github.com>
2025-04-07 23:29:22 -0500

    feat: agregado proyeccion y agrupacion de ventas para factura

--- desnormalizacion/comandos.js
+++ desnormalizacion/comandos.js
@@ -16,10 +16,32 @@ db.invoices.aggregate([
     {
         $project: { "details.productId": 0, "details.product.price": 0, "details.product._id": 0}
     },
+])
+
+db.invoices.aggregate([
+    { $match: { invoiceNumber: 1}},
+    { $unwind: "$details"},
+    {
+        $lookup: {
+            from: "products",
+            localField: "details.productId",
+            foreignField: "code",
+            as: "details.product",
+        }
+    },
+    {
+        $addFields: { "details.totalPerProduct":
+          { $multiply: [ "$details.price", "$details.quantity"] } }
+    },
+    {
+        $project: { "details.productId": 0, "details.product.price": 0, "details.product._id": 0}
+    },
     {
         $group: {
-            _id: $invoiceNumber
+            _id: {invoiceNumber: "$invoiceNumber", date: "$date", customer: "$customer", seller: "$seller"}, details: { $push: "$details"} 
         }
+    },
+    {
+        $project: { _id: 0, invoiceNumber: "$_id.invoiceNumber", date: "$_id.date", customer: "$_id.customer", seller: "$_id.seller", details: 1}
     }
-
 ])
